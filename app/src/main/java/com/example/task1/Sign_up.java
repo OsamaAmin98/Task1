b5bb043a742b04e61edbf8481d197851af9967bf
Osama Amin <osamaamin141@gmail.com>
2020-02-18 07:21:56 +0200

    Sign up and Sign in ICONS , IDs , SCROLLVIEW

--- app/src/main/java/com/example/task1/Sign_up.java
+++ app/src/main/java/com/example/task1/Sign_up.java
@@ -7,22 +7,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class Sign_in extends AppCompatActivity {
+public class Sign_up extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_sign_up);
 
-        TextView signUp = findViewById(R.id.text_view_sign_up);
 
-        signUp.setOnClickListener(new View.OnClickListener() {
+
+      TextView signIn = findViewById(R.id.text_view_sign_in);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Sign_in.this, Sign_up.class);
+
+                Intent intent = new Intent(Sign_up.this,Sign_in.class);
                 startActivity(intent);
 
             }
         });
+
+
+
+
+
     }
 }
